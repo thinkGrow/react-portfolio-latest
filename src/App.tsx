ff13b70440649675d09ccd80586bb7ae1cae3926
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-18 04:36:37 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/retroui/Button";
+
+function App() {
+  return (
+    <>
+      <Button>Click Me!</Button>;
+    </>
+  );
+}
+
+export default App;
